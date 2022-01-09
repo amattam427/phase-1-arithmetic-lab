13a29b16d09d8be3f15fc9d1126cbed5c69f0492
Ashley Mattam <amattam427@gmail.com>
2022-01-08 21:14:15 -0500

    commit tree e6986aa4cefed544be7d2d9317fd41b6ce314bb4

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code here
-let num1=31;
-let num2=2;
-const multiply = num1*num2
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2;
 
-const random = Math.floor(Math.random() * 10) + 1; random;
+let random = Math.floor(Math.random() * 10) + 1
 
-let num3=16;
-let num4=4;
-const mod=num3/num4
+let num3 = 16
+let num4 = 4
+const mod = num3/num4;
 
-const max = Math.max(5,10,15,20);
+const max = Math.max(5, 10, 15, 20);
